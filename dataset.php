82baf76b20370dfb7063b78b12751bd400477a94
Leonidas Kallipolitis <l.kallipolitis@atc.gr>
2014-01-08 15:02:00 +0200

    retrieve city id in filters array

--- dataset.php
+++ dataset.php
@@ -1,5 +1,4 @@
 <?php
-session_start();
 header('Content-type: application/json; charset=utf-8');
 header("Expires: Tue, 01 Jan 2000 00:00:00 GMT");
 header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
@@ -20,52 +19,59 @@ $filters = array();
 $keys = array();
 
 if (isset($_GET['uid'])) {
-  Database::connect();
+    Database::connect();
 
-  if ($app = App::createFromDb($_GET['uid'])) {
-    $app_datasets = array();
-    $name = $app->name;
+    if ($app = App::createFromDb($_GET['uid'])) {
+        $app_datasets = array();
+        $name = $app->name;
 
-    foreach ($app->datasetIds as $key => $value) {
+        foreach ($app->datasetIds as $value) {
 
-      $sql = "SELECT * FROM datasets WHERE id =" . $value;
+            $sql = "SELECT datasets.type, datasets.url, city_datasets.city_id AS cityId FROM datasets JOIN 
+                city_datasets ON city_datasets.dataset_id = datasets.id WHERE datasets.id=" . $value;
 
-      /* If we are behind a proxy, we need to
-       * setup a context for file_get_contents
-       */
-      if (PROXYUSE) {
-        $aContext = array(
-          'http' => array(
-            'proxy' => 'tcp://' . PROXYNAME . ':' . PROXYPORT,
-            'request_fulluri' => true,
-          ),
-        );
-        $cxContext = stream_context_create($aContext);
-      }
-      else
-        $cxContext = null;
-      foreach (Database::$dbh->query($sql) as $row) {
-        $type = $row['type'];
-        $json = file_get_contents($row['url'], False, $cxContext);
-        $assocArray = json_decode($json, true);
-        $poisDataset = ResponseDataset::createFromArray(DatasetTypes::Poi, $assocArray);
-        foreach ($poisDataset->poi as $poi) {
-          foreach ($poi->category as $cat) {
-            if (!in_array($cat, $keys)) {
-              $keys[] = $cat;
-              $filters[] = new Filter($cat, true, $type);
+            /* If we are behind a proxy, we need to
+             * setup a context for file_get_contents
+             */
+            if (PROXYUSE) {
+                $aContext = array(
+                    'http' => array(
+                        'proxy' => 'tcp://' . PROXYNAME . ':' . PROXYPORT,
+                        'request_fulluri' => true,
+                    ),
+                );
+                $cxContext = stream_context_create($aContext);
             }
-          }
+            else
+                $cxContext = null;
+            foreach (Database::$dbh->query($sql) as $row) {
+                $type = $row['type'];
+
+                $json = file_get_contents($row['url'], False, $cxContext);
+                $assocArray = json_decode($json, true);
+                $poisDataset = ResponseDataset::createFromArray(DatasetTypes::Poi, $assocArray);
+
+                foreach ($poisDataset->poi as $poi) {
+                    foreach ($poi->category as $cat) {
+                        if (!in_array($cat, $keys)) {
+                            $keys[] = $cat;
+                            if ($cat === reset($keys)) {
+                                $filters[0] = new Filter($cat, true, $type, $row['cityId'], true);
+                            } else {
+                                $filters[] = new Filter($cat, false, $type, $row['cityId'], true);
+                            }
+                        }
+                    }
+                }
+                array_push($app_datasets, new ResponseDataset($poisDataset, $name));
+            }
         }
-        array_push($app_datasets, new ResponseDataset($poisDataset, $name));
-      }
+        Util::printJsonObj(new Response($app_datasets, $name, $filters));
     }
-    Util::printJsonObj(new Response($app_datasets, $name, $filters));
-  }
-  else
-    Util::printJsonObj(new ResponseError("failed", "invalid application id"));
-  Database::disconnect();
+    else
+        Util::printJsonObj(new ResponseError("failed", "invalid application id"));
+    Database::disconnect();
 }
 else
-  Util::printJsonObj(new ResponseError("failed", "missing application id"));
+    Util::printJsonObj(new ResponseError("failed", "missing application id"));
 ?>
