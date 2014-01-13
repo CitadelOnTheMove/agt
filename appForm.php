7ada51788e4467d8932c971b7ecfe47eed54f70a
Leonidas Kallipolitis <l.kallipolitis@atc.gr>
2014-01-13 17:01:02 +0200

    fixed validation

--- appForm.php
+++ appForm.php
@@ -58,62 +58,62 @@ $username = $user['username'];
             var counterMax = 0;
 
             $(document).ready(function() {
-              
-              $('.ui-checkbox a').bind("click", function( event, data ){
-                event.stopPropagation();             
-                window.open($this.attr('href'), $this.attr('target'));     
-              }); 
 
-             $("input[type=checkbox][id^=city]").click(function() {
-               
-               /* The id attribute is of the form "cityXX"
-                * where XX is the id of the city
-                */ 
-               var cityId = $(this).attr('id').substring(4);  
-               
-               if($(this).attr('checked')){
-                
-                /* Check if we allready have the datasets from previous 
-                 * calls and if yes show them.
-                 */
-                if($('input[type=checkbox][name="datasetIds[]"].city'+cityId).length)
-                {
-                       $('input[type=checkbox][name="datasetIds[]"].city'+cityId).parent().show();
-                }
-                else{                                
-                  $.ajax({
-                         type: "GET",
-                         url: "ajaxHelper.php?action=getCities&cityId=" + cityId,
-                         cache: false,
-                         success: onSuccess,
-                         error: onFailure
-                    }); 
-                }
-               } // if 
-               else{ 
-                 /*city checkbox unchecked, hide and uncheck the checkboxes */
-                 $('input[type=checkbox][name="datasetIds[]"].city'+cityId).parent().hide();
-                 $('input[type=checkbox][name="datasetIds[]"].city'+cityId).attr("checked", false).checkboxradio("refresh");
-               }
-             }); // click
-                
+                $('.ui-checkbox a').bind("click", function(event, data) {
+                    event.stopPropagation();
+                    window.open($this.attr('href'), $this.attr('target'));
+                });
+
+                $("input[type=checkbox][id^=city]").click(function() {
+
+                    /* The id attribute is of the form "cityXX"
+                     * where XX is the id of the city
+                     */
+                    var cityId = $(this).attr('id').substring(4);
+
+                    if ($(this).attr('checked')) {
+
+                        /* Check if we allready have the datasets from previous 
+                         * calls and if yes show them.
+                         */
+                        if ($('input[type=checkbox][name="datasetIds[]"].city' + cityId).length)
+                        {
+                            $('input[type=checkbox][name="datasetIds[]"].city' + cityId).parent().show();
+                        }
+                        else {
+                            $.ajax({
+                                type: "GET",
+                                url: "ajaxHelper.php?action=getCities&cityId=" + cityId,
+                                cache: false,
+                                success: onSuccess,
+                                error: onFailure
+                            });
+                        }
+                    } // if 
+                    else {
+                        /*city checkbox unchecked, hide and uncheck the checkboxes */
+                        $('input[type=checkbox][name="datasetIds[]"].city' + cityId).parent().hide();
+                        $('input[type=checkbox][name="datasetIds[]"].city' + cityId).attr("checked", false).checkboxradio("refresh");
+                    }
+                }); // click
+
             }); // document.ready
 
             function onSuccess(data, status)
-            { 
-               /* Add the new datasets to the existing ones */
+            {
+                /* Add the new datasets to the existing ones */
                 //var currentContent = $('#datasetsCheckboxes').html();
                 $('#datasetsCheckboxes').append(data);
 
                 /* Apply the checkbox styling to the new ones */
                 $('input[type=checkbox][name="datasetIds[]"]').each(function() {
-                  
-                     if($(this).parent().not('.ui-checkbox').length)
+
+                    if ($(this).parent().not('.ui-checkbox').length)
                         $(this).checkboxradio().trigger('create');
-                });               
-              
-                $("input[type=checkbox][id^=city]").checkboxradio('enable');       
-              }
+                });
+
+                $("input[type=checkbox][id^=city]").checkboxradio('enable');
+            }
 
             function onFailure(data, status)
             {
@@ -124,133 +124,135 @@ $username = $user['username'];
 
     </head>
     <body>
-      <div data-role="page">
-        <div data-role="header">
-          <h1>Create your app</h1>
-        </div>
+        <div data-role="page">
+            <div data-role="header">
+                <h1>Create your app</h1>
+            </div>
 
-        <div data-role="content"> 
+            <div data-role="content"> 
 
-          <div id="createNewAppWrapper">
-            <?php
-            include_once 'Config.php';
-            include_once CLASSES . 'Database.class.php';
-            include_once CLASSES . 'App.class.php';
+                <div id="createNewAppWrapper">
+                    <?php
+                    include_once 'Config.php';
+                    include_once CLASSES . 'Database.class.php';
+                    include_once CLASSES . 'App.class.php';
 
-            Database::connect();
+                    Database::connect();
 
-            // define variables and set to empty values
-            $darkColorErr = $colorErr = $nameErr = $cityIdsErr = $datasetIdsErr = "";           
+                    // define variables and set to empty values
+                    $darkColorErr = $colorErr = $nameErr = $cityIdsErr = $datasetIdsErr = "";
 
-            $error = false;
-            // Check all required fields
-            if ($_SERVER["REQUEST_METHOD"] == "POST") {
+                    $error = false;
+                    // Check all required fields
+                    if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
-              if (empty($_POST["cityIds"])) {
-                $cityIdsErr = "City is required";
-                $error = true;
-              }
-              if (empty($_POST["datasetIds"])) {
-                $datasetIdsErr = "Dataset is required";
-                $error = true;
-              }
-              if (empty($_POST["name"])) {
-                $nameErr = "Application Name is required";
-                $error = true;
-              }
-              if (empty($_POST["color"])) {
-                $colorErr = "Color is required";
-                $error = true;
-              }
-              if (empty($_POST["darkColor"])) {
-                $darkColorErr = "Dark Color is required";
-                $error = true;
-              }             
+                        if (empty($_POST["cityIds"])) {
+                            $cityIdsErr = "City is required";
+                            $error = true;
+                        }
+                        if (empty($_POST["datasetIds"])) {
+                            $datasetIdsErr = "Dataset is required";
+                            $error = true;
+                        }
+                        if (empty($_POST["name"])) {
+                            $nameErr = "Application Name is required";
+                            $error = true;
+                        }
+                        if (empty($_POST["color"])) {
+                            $colorErr = "Color is required";
+                            $error = true;
+                        }
+                        if (empty($_POST["darkColor"])) {
+                            $darkColorErr = "Dark Color is required";
+                            $error = true;
+                        }
 
-              if (!$error) {
-                if ((isset($_POST["cityIds"])) && (isset($_POST["name"])) &&
-                    (isset($_POST["datasetIds"])) && (isset($_POST["color"])) &&
-                    (isset($_POST["darkColor"]))) {
+                        if (!$error) {
+                            if ((isset($_POST["cityIds"])) && (isset($_POST["name"])) &&
+                                    (isset($_POST["datasetIds"])) && (isset($_POST["color"])) &&
+                                    (isset($_POST["darkColor"]))) {
 
-                  Database::begin();
+                                Database::begin();
                                 $newApp = App::createFromArray($_POST, $userId);
-                  if ($newApp->save()) {
-                    Database::commit();
-                    echo '<div class="success">Your application was created successfully!';
-                    echo '<br><br>';
-                    echo ' <a href="index.php?uid=' . $newApp->uid . '" target="_blank" rel="external">See my app!</a>';
-                    echo ' <a style="float:right" href="appForm.php" target="_blank">Create a new app</a></div>';
-                  }
-                  else {
-                    Database::rollback();
-                    echo '<div class="failure">Your application was not created.';
-                    echo '<br><br>';
-                    echo ' <a style="float:right" href="appForm.php" target="_blank">Create a new app</a></div>';
-                  }
-                }
-              }
-            }
+                                if ($newApp->save()) {
+                                    Database::commit();
+                                    echo '<div class="success">Your application was created successfully!';
+                                    echo '<br><br>';
+                                    echo ' <a href="index.php?uid=' . $newApp->uid . '" target="_blank" rel="external">See my app!</a>';
+                                    echo ' <a style="float:right" href="appForm.php" target="_blank">Create a new app</a></div>';
+                                } else {
+                                    Database::rollback();
+                                    echo '<div class="failure">Your application was not created.';
+                                    echo '<br><br>';
+                                    echo ' <a style="float:right" href="appForm.php" target="_blank">Create a new app</a></div>';
+                                }
+                            }
+                        }
+                    }
 
-            function clear_input($data) {
-              $data = trim($data);
-              $data = stripslashes($data);
-              $data = htmlspecialchars($data);
-              return $data;
-            }
-            ?>
+                    function clear_input($data) {
+                        $data = trim($data);
+                        $data = stripslashes($data);
+                        $data = htmlspecialchars($data);
+                        return $data;
+                    }
+                    ?>
 
-            <?php if ($_SERVER["REQUEST_METHOD"] != "POST") { ?>
-              <form id="createNewAppForm"  method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
+                    <?php if ($_SERVER["REQUEST_METHOD"] != "POST" || $error ) { ?>
+                        <form id="createNewAppForm"  method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
                              <p><?php echo 'Hi <b>' . $username . '</b>! Use this form to create your own app.' ?></p>
-                <p><span class="error">* required field.</span></p>                        
+                            <p><span class="error">* required field.</span></p>                        
 
-                <legend><b>Select cities:</b> <span class="error">* <?php echo $cityIdsErr; ?></span></legend><br/>
-                <div id="citiesCheckboxes"  data-role="controlgroup">
+                            <legend><b>Select cities:</b> <span class="error">* <?php echo $cityIdsErr; ?></span></legend><br/>
+                            <div id="citiesCheckboxes"  data-role="controlgroup">
 
-                  <?php
-                  $sql1 = 'SELECT * FROM cities ORDER BY name';
-                  foreach (Database::$dbh->query($sql1) as $row) {
-                    echo '<input type="checkbox" autocomplete="off" name="cityIds[]" id="city' . $row['id'] . '"  value="' . $row['id'] . '">
+                                <?php
+                                $sql1 = 'SELECT * FROM cities ORDER BY name';
+                                foreach (Database::$dbh->query($sql1) as $row) {
+                                    echo '<input type="checkbox" autocomplete="off" name="cityIds[]" id="city' . $row['id'] . '"  value="' . $row['id'] . '">
                             <label for="city' . $row['id'] . '">' . $row['name'] . '</label>';
-                  }
-                  ?>
-                </div>
+                                }
+                                ?>
+                            </div>
 
-                <br><br>
+                            <br><br>
 
-                <legend><b>Select datasets:</b> <span class="error">* <?php echo $datasetIdsErr; ?></span> </legend><br/>
+                            <legend><b>Select datasets:</b> <span class="error">* <?php echo $datasetIdsErr; ?></span> </legend><br/>
 
-                <div id="datasetsCheckboxes" >
-                  <?php
-                  echo '<i>Select a City to see the available datasets.</i>'
-                  ?>
-                </div>
+                            <div id="datasetsCheckboxes" >
+                                <?php
+                                echo '<i>Select a City to see the available datasets.</i>'
+                                ?>
+                            </div>
 
-                <br>
+                            <br>
                 <a href="/<?php echo BASE_DIR?>importDatasets.php">Click here if you want to import a new dataset</a>
-                <br><br>
+                            <br><br>
 
-                <legend><b>Select the basic color of your app:</b><span class="error">* <?php echo $colorErr; ?></span></legend><br/>
-                <input type="color" class="full" name="color">
 
-                <br><br>
+                            <legend><b>Select the basic color of your app:</b><span class="error">* <?php echo $colorErr; ?></span></legend><br/>
+                            <input type="color" class="full" name="color" required>
 
-                <legend><b>Select the secondary color (active/hovered buttons):</b><span class="error">* <?php echo $darkColorErr; ?></span></legend><br/>
-                <input type="color" class="full" name="darkColor">
-                <br><br>
+                            <br><br>
 
-                <legend><b>Application Name:</b> <span class="error">* <?php echo $nameErr; ?></span></legend><br/>
-                <input type="text" name="name">
-                <br><br>
-                <br><br>
-                <input type="submit" name="submit" value="Create the app">
-              </form>
+                            <legend><b>Select the secondary color (active/hovered buttons):</b><span class="error">* <?php echo $darkColorErr; ?></span></legend><br/>
 
-            <?php } // end if 
-            Database::disconnect();?>
-          </div>
+                            <input type="color" class="full" name="darkColor" required>
+                            <br><br>
+
+                            <legend><b>Application Name:</b> <span class="error">* <?php echo $nameErr; ?></span></legend><br/>
+                            <input type="text" name="name" required>
+                            <br><br>
+                            <br><br>
+                            <input type="submit" name="submit" value="Create the app">
+                        </form>
+
+                    <?php } // end if 
+                    Database::disconnect();
+                    ?>
+                </div>
+            </div>
         </div>
-      </div>
     </body>
 </html>
 
