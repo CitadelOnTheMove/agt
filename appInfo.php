346355059a925637f388a7396c55d01256286bdd
Leonidas Kallipolitis <l.kallipolitis@atc.gr>
2014-01-21 14:31:13 +0200

    added filtering by user

--- appInfo.php
+++ appInfo.php
@@ -9,22 +9,39 @@ include_once CLASSES . 'App.class.php';
 include_once CLASSES . 'AppInfo.class.php';
 
 $dashbord = array();
-
 Database::connect();
-$sql = "SELECT apps.uid, apps.name, apps.created, users.username from apps 
-       LEFT JOIN users On users.id = apps.userId";
 
-$sth = Database::$dbh->query($sql);
-$results = $sth->fetchAll(PDO::FETCH_ASSOC);
-foreach ($results as $row) {
-    $appId = $row['uid'];
-    $appName = $row['name'];
-    $created = $row['created'];
-    $username = $row['username'];
+if (isset($_GET['username'])) {
+    $username = $_GET['username'];
+    $sql = "SELECT apps.uid, apps.name, apps.created, users.username from apps 
+        LEFT JOIN users On users.id = apps.userId WHERE users.username = :username";
+} else {
+    $username = "";
+    $sql = "SELECT apps.uid, apps.name, apps.created, users.username from apps 
+        LEFT JOIN users On users.id = apps.userId";
+}
+
+try {
+    $sqlParams[":username"] = $username;
+    $sth = Database::$dbh->prepare($sql);
+    $sth->execute($sqlParams);
+    $results = $sth->fetchAll(PDO::FETCH_ASSOC);
+
+    foreach ($results as $row) {
+        $appId = $row['uid'];
+        $appName = $row['name'];
+        $created = $row['created'];
+        $username = $row['username'];
 
     $appUrl = SERVERNAME . BASE_DIR . "index.php" . '?uid=' . $appId;
 
-    $dashbord[] = new AppInfo($appUrl, $appName, $created, $username);
+        $dashbord[] = new AppInfo($appUrl, $appName, $created, $username);
+    }
+} catch (Exception $e) {
+    if (DEBUG)
+        $sth->debugDumpParams();
+    Util::throwException(__FILE__, __LINE__, __METHOD__, "query database failed", $e->getMessage(), $e);
+    return false;
 }
 Database::disconnect();
 
