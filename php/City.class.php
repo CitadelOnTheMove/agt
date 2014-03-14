d110a84e791428dc7f9a7dbe44483167346eb2c2
Leonidas Kallipolitis <l.kallipolitis@atc.gr>
2014-03-14 12:01:02 +0200

    added datasets related to the city (per app)

--- php/City.class.php
+++ php/City.class.php
@@ -6,12 +6,15 @@ class City {
     public $name;
     public $lat;
     public $lon;
+    public $datasets;
 
-    public function __construct($id, $name, $lat, $lon) {
+    public function __construct($id, $name, $lat, $lon, $datasets) {
         $this->id = $id;
         $this->name = $name;
         $this->lat = $lat;
         $this->lon = $lon;
+        $this->datasets = $datasets;
+        
     }
 
     public static function saveNewCity($cityName, $latitude, $longitude) {
@@ -25,16 +28,15 @@ class City {
 
                 $sql1 = "INSERT INTO cities VALUES(null, :city, :latitude, :longitude)";
                 $sqlParams1 = array(':city' => $cityName,
-                    ':latitude' => round($latitude, 6)  ,
+                    ':latitude' => round($latitude, 6),
                     ':longitude' => round($longitude, 6));
 
                 try {
                     $sth2 = Database::$dbh->prepare($sql1);
                     $sth2->execute($sqlParams1);
-                    
+
                     $newCityId = Database::$dbh->lastInsertId();
                     return $newCityId;
-                    
                 } catch (Exception $e) {
                     if (DEBUG)
                         $sth2->debugDumpParams();
