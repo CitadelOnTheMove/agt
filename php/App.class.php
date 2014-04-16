74dd112630a25e680159792da87cb2fc902ed111
Leonidas Kallipolitis <l.kallipolitis@atc.gr>
2014-04-16 15:25:46 +0300

    moved guid function to Util

--- php/App.class.php
+++ php/App.class.php
@@ -127,7 +127,7 @@ class App {
      */
     public function save() {
 
-        $this->uid = trim(App::getGUID(), '{}');
+        $this->uid = trim(Util::getGUID(), '{}');
         $sql = "INSERT INTO apps VALUES(:uid, :name, :userId, Now(), :description, :image, :isDeleted)";
         $sqlParams = array(':uid' => $this->uid, ':name' => $this->name, ':userId' => $this->userId, ':description' => $this->description, ':image' => $this->image, ':isDeleted' => 0);
         try {
@@ -201,25 +201,6 @@ class App {
         }
 
         return true;
-    }
-
-    /* Generate GUID in every environment */
-    public static function getGUID() {
-        if (function_exists('com_create_guid')) {
-            return com_create_guid(); /* windows */
-        } else {
-            mt_srand((double) microtime() * 10000); //optional for php 4.2.0 and up.
-            $charid = strtoupper(md5(uniqid(rand(), true)));
-            $hyphen = chr(45); // "-"
-            $uuid = chr(123)// "{"
-                    . substr($charid, 0, 8) . $hyphen
-                    . substr($charid, 8, 4) . $hyphen
-                    . substr($charid, 12, 4) . $hyphen
-                    . substr($charid, 16, 4) . $hyphen
-                    . substr($charid, 20, 12)
-                    . chr(125); // "}"
-            return $uuid;
-        }
     }
 
 }
