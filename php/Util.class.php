81dee213b18d66c17532e42740b5358eb9795ee4
Leonidas Kallipolitis <l.kallipolitis@atc.gr>
2014-04-16 15:26:12 +0300

    moved guid function to Util

--- php/Util.class.php
+++ php/Util.class.php
@@ -69,6 +69,25 @@ class Util {
             Util::throwException(__FILE__, __LINE__, __METHOD__, "get next increment from pois", $e->getMessage(), $e);
         }
     }
+    
+     /* Generate GUID in every environment */
+    public static function getGUID() {
+        if (function_exists('com_create_guid')) {
+            return com_create_guid(); /* windows */
+        } else {
+            mt_srand((double) microtime() * 10000); //optional for php 4.2.0 and up.
+            $charid = strtoupper(md5(uniqid(rand(), true)));
+            $hyphen = chr(45); // "-"
+            $uuid = chr(123)// "{"
+                    . substr($charid, 0, 8) . $hyphen
+                    . substr($charid, 8, 4) . $hyphen
+                    . substr($charid, 12, 4) . $hyphen
+                    . substr($charid, 16, 4) . $hyphen
+                    . substr($charid, 20, 12)
+                    . chr(125); // "}"
+            return $uuid;
+        }
+    }
 
 }
 
